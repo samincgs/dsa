865a6abb8552cb2c21c1f47c904e9017bd3c51e3
samin <samincgs@gmail.com>
2024-10-15 20:43:19 -0400

    started learning about array lists

---
+++ array_list.py
@@ -0,0 +1,4 @@
+# Array Lists (aka dynamic arrays, growable arrays, resizable arrays, dynamic tables, mutable arrays)
+# arrays inheritly dont have an add or delete function
+# Arrays Lists server as wrappers for arrays and will manipulate an underlying array to give an array some extra features
+# A python List is closer to an array list then an array
