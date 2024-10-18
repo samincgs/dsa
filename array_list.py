e4cba684aa1e59329a4e561cdb621d8db7fe647d
samin <samincgs@gmail.com>
2024-10-18 16:09:32 -0400

    coded a c implementation of an array list

--- array_list.py
+++ array_list.py
@@ -15,12 +15,53 @@ Array List Time Complexity
 - read -> O(1)
 '''
 
-''' C implementation of an Array List
-#include <stdio.h> 
-#include <stdlib.h>
+#C implementation of an Array List
+include <stdio.h> 
+include <stdlib.h>
+
+define BASE_CAPACITY 10
+
+typedef struct ArrayList {
+    int *data; (pointing to the beginning of the array of integers)
+    int size; (current amount of items)
+    int capacity; (how much the array size is gonna start at)
+} al;
+
+al * create_al(void) {
+    al * new_al = malloc(sizeof(al));
+    new_al->capacity = BASE_CAPACITY;
+    new_al->size = 0;
+    new_al->data = calloc(new_al->capacity, sizeof(int));
+    return new_al;
+}
+
+void append_al(al * target_al, int val) {
+    target_al->data[target_al->size] = val;
+    target_al->size++;
+    if (target_al->size == target_al->size) {
+        printf("Realloc %d\n", target_al->capacity);
+        target_al->capacity *= 2;
+        target_al->data = realloc(target_al->data, sizeof(int) * target_al->capacity);
+    }
+}
+
+void print_al(al * target_al) {
+    for (int i = 0; i < target_al->size; i++) {
+        printf("%d\n", target_al->data[i]);
+    }
+    printf("\n");
+}
 
 int main(void) {
+    al* my_array_list = create_al();
+    
+    append_al(my_array_list, 2);
+    append_al(my_array_list, 5);
+    append_al(my_array_list, 3);
+    
+    print_al(my_array_list);
+    
+    printf("%d\n", my_array_list->capacity);
     return 0;
 }
 
-'''
