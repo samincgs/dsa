678385ab07898babbe19a4938376d3e84897ff1d
samin <samincgs@gmail.com>
2025-01-13 22:46:50 -0500

    continuing dsa study

---
+++ dsa.py
@@ -0,0 +1,5 @@
+# Data Structures & Algorithms
+# why is algorithms always associated? it is because algorithms process the data, that data is stored in a data structure
+
+# Algorithms: Just a process of doing something (typically uses a function to carry out a particular solution)
+# Data: Is the structured data being passed into the function
